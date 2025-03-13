93c95d081638a8da9005f05cd691cdb9d399e555
GalaxyInfluence <139978578+NotKFC-Real@users.noreply.github.com>
2025-03-13 18:14:46 +0000

    Update homepage.tsx

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -112,11 +112,6 @@ export function Homepage() {
           </a>
         </div>
       </div>
-      <footer className="mt-8 text-sm text-white/70 flex items-center justify-center">
-        <span className="mr-1">Inspired by Lewisakura&apos;s </span>
-        <a href="https://github.com/lewisakura/webhook-proxy" className="hover:underline mr-4">Webhook Proxy</a>
-        <span>Made with ❤️ by <a target = "_blank" href="https://starvsk.dev">StarVSK</a></span>
-      </footer>
     </div>
   )
 }
